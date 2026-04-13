cbaf677e8d3dd5c708850fc0249f47341aa7933a
Baadal-git <kolhadesidhant@gmail.com>
2026-04-13 15:34:51 +0530

    Revert middleware

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,19 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverExternalPackages: ['@clerk/backend', '@clerk/shared'],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      crypto: false,
-    }
-    config.resolve.conditionNames = [
-      'edge-light',
-      'worker',
-      ...config.resolve.conditionNames ?? [],
-    ]
-    return config
+    typedRoutes: false,
   },
 }
 
